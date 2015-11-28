02c15b4a0e8a4230a3fbe52b8c961ccebc4ca3bd
Felix Sterzelmaier pc <f.sterzelmaier@web.de>
2015-11-28 03:47:08 +0100

    deletions

--- dist/id3-0.2/examples/example_version.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-echo "from file\n";
-$v= id3_get_version('/home/schst/demo.mp3');
-var_dump($v);
-
-echo "from resource\n";
-$v = id3_get_version(fopen('/home/schst/demov2.mp3', 'rb'));
-var_dump($v);
-?>
