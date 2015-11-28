dd22b4866bc84819fef6291ed37a0874833a45de
Felix Sterzelmaier pc <f.sterzelmaier@web.de>
2015-11-28 04:27:24 +0100

    suche angefangen

--- php-vote/php/ajax.php
+++ php-vote/php/ajax.php
@@ -15,6 +15,14 @@ switch($_GET["action"]) {
         if($r===false) doError("Error while getting highscore");
         else doOutput($r,"showhighscore");
         break;
+    case("search"): //TODO keyword post, mit ajax
+        if(!isset($_GET["keyword"])) {
+            doError("No keyword specified");
+        }
+        $r = doSearch($_GET["keyword"]);
+        if($r===false) doError("Error while getting highscore");
+        else doOutput($r,"search");
+        break;
     default: doError("No valid action specified");
 
 }
--- php-vote/php/functions.php
+++ php-vote/php/functions.php
@@ -9,11 +9,22 @@ function doError($e) {
 }
 
 //todo nur zählen, wenn vote nicht vor einem Eintrag in playlog ist.
-//todo mit fileinfos und Foldername + Folderpic + Pfad
+//todo mit fileinfos
 function doShowhighscore() {
     $stmt = $GLOBALS["db"]->prepare("SELECT fileid,COUNT(*) as anzahl FROM votes GROUP BY fileid");
     $tmp = array();
     if($stmt->execute()) {
+        while ($row = $stmt->fetchObject()) {
+            $tmp[] = $row;
+        }
+        return $tmp;
+    } doError("Highscore db query failed");
+}
+
+function doSearch($keyword) {
+    $stmt = $GLOBALS["db"]->prepare("SELECT * FROM files WHERE filename LIKE :d OR artist LIKE :d OR title LIKE :d OR album LIKE :d");
+    $tmp = array();
+    if($stmt->execute(array(":d" => "%".$keyword."%"))) {
         while ($row = $stmt->fetchObject()) {
             $tmp[] = $row;
         }
